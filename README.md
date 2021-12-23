6a219599aa154e2f48d4fd086fcea902aa0b0f56
Phil Wang <lucidrains@gmail.com>
2021-12-23 12:57:00 -0800

    first working sketch of architecture

--- README.md
+++ README.md
@@ -2,7 +2,117 @@
 
 A repository with exploration into using transformers to predict DNA ↔ transcription factor binding. If you are a researcher interested in this problem, please join the <a href="https://discord.gg/s7WyNU24aM">discord server</a> for discussions.
 
+## Install
+
+```bash
+$ pip install tf-bind-transformer
+```
+
+## Usage
+
+```python
+import torch
+from tf_bind_transformer import Model
+
+# instantiate enformer or load pretrained
+
+from enformer_pytorch import Enformer
+enformer = Enformer(
+    dim = 1536,
+    depth = 2,
+    target_length = 256
+)
+
+# instantiate model wrapper that takes in enformer
+
+model = Model(
+    enformer = enformer,
+    aa_embed_dim = 512,
+    contextual_embed_dim = 256
+).cuda()
+
+# mock data
+
+seq = torch.randint(0, 4, (1, 196_608 // 2)).cuda() # for ACGT
+
+aa_embed = torch.randn(1, 1024, 512).cuda()
+aa_mask = torch.ones(1, 1024).bool().cuda()
+
+contextual_embed = torch.randn(1, 256).cuda() # contextual embeddings, including cell type, species, experimental parameter embeddings
+
+target = torch.randn(1, 256).cuda()
+
+# train
+
+loss = model(
+    seq,
+    aa_embed = aa_embed,
+    aa_mask = aa_mask,
+    contextual_embed = contextual_embed,
+    target = target
+)
+
+loss.backward()
+
+# after a lot of training
+
+corr_coef = model(
+    seq,
+    aa_embed = aa_embed,
+    aa_mask = aa_mask,
+    contextual_embed = contextual_embed,
+    target = target,
+    return_corr_coef = True
+)
+
+print(corr_coef.shape)
+```
+
+## Todo
+
+- [ ] ESM and AF2 embedding fetching integrations
+- [ ] allow for fine-tuning layernorms of Enformer easily
+- [ ] normalization of interactions between genetic and amino acid sequence
+- [ ] hyperparameters for different types of normalization on fine grained interactions feature map
 
 ## Appreciation
 
 This work is generously sponsored by <a href="https://github.com/jeffhsu3">Jeff Hsu</a> to be done completely open sourced.
+
+## Citations
+
+```bibtex
+@article {Avsec2021.04.07.438649,
+    author  = {Avsec, {\v Z}iga and Agarwal, Vikram and Visentin, Daniel and Ledsam, Joseph R. and Grabska-Barwinska, Agnieszka and Taylor, Kyle R. and Assael, Yannis and Jumper, John and Kohli, Pushmeet and Kelley, David R.},
+    title   = {Effective gene expression prediction from sequence by integrating long-range interactions},
+    elocation-id = {2021.04.07.438649},
+    year    = {2021},
+    doi     = {10.1101/2021.04.07.438649},
+    publisher = {Cold Spring Harbor Laboratory},
+    URL     = {https://www.biorxiv.org/content/early/2021/04/08/2021.04.07.438649},
+    eprint  = {https://www.biorxiv.org/content/early/2021/04/08/2021.04.07.438649.full.pdf},
+    journal = {bioRxiv}
+}
+```
+
+```bibtex
+@misc{yao2021filip,
+    title   = {FILIP: Fine-grained Interactive Language-Image Pre-Training},
+    author  = {Lewei Yao and Runhui Huang and Lu Hou and Guansong Lu and Minzhe Niu and Hang Xu and Xiaodan Liang and Zhenguo Li and Xin Jiang and Chunjing Xu},
+    year    = {2021},
+    eprint  = {2111.07783},
+    archivePrefix = {arXiv},
+    primaryClass = {cs.CV}
+}
+```
+
+```bibtex
+@misc{tay2020hypergrid,
+    title   = {HyperGrid: Efficient Multi-Task Transformers with Grid-wise Decomposable Hyper Projections},
+    author  = {Yi Tay and Zhe Zhao and Dara Bahri and Donald Metzler and Da-Cheng Juan},
+    year    = {2020},
+    eprint  = {2007.05891},
+    archivePrefix = {arXiv},
+    primaryClass = {cs.CL}
+}
+```
--- tf_bind_transformer/tf_bind_transformer.py
+++ tf_bind_transformer/tf_bind_transformer.py
@@ -1,10 +1,132 @@
+import math
 import torch
+import torch.nn.functional as F
 from torch import nn, einsum
-from einops import rearrange
+
+from einops import rearrange, reduce
+from einops.layers.torch import Rearrange
+
+from enformer_pytorch import Enformer
+
+# helper functions
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+def log(t, eps = 1e-20):
+    return torch.log(t + eps)
+
+def l2norm(t):
+    return F.normalize(t, dim = -1)
+
+# losses and metrics
+
+def poisson_loss(pred, target):
+    return (pred - target * log(pred)).mean()
+
+def pearson_corr_coef(x, y, eps = 1e-8):
+    x2 = x * x
+    y2 = y * y
+    xy = x * y
+    ex = x.mean(dim = 1)
+    ey = y.mean(dim = 1)
+    exy = xy.mean(dim = 1)
+    ex2 = x2.mean(dim = 1)
+    ey2 = y2.mean(dim = 1)
+    r = (exy - ex * ey) / (torch.sqrt(ex2 - (ex * ex)) * torch.sqrt(ey2 - (ey * ey)) + eps)
+    return r.mean(dim = -1)
+
+# model
 
 class Model(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        *,
+        enformer,
+        enformer_dim = 1536,
+        latent_dim = 64,
+        latent_heads = 32,
+        aa_embed_dim = 512,
+        contextual_embed_dim = 256,
+    ):
         super().__init__()
+        assert isinstance(enformer, Enformer), 'enformer must be an instance of Enformer'
+        self.enformer = enformer
 
-    def forward(self, x):
-        return x
+        self.latent_heads = latent_heads
+        inner_latent_dim = latent_heads * latent_dim
+
+        self.seq_embed_to_latent_w = nn.Parameter(torch.randn(enformer_dim * 2, inner_latent_dim))
+        self.seq_embed_to_latent_b = nn.Parameter(torch.randn(inner_latent_dim))
+
+        self.aa_seq_embed_to_latent_w = nn.Parameter(torch.randn(aa_embed_dim, inner_latent_dim))
+        self.aa_seq_embed_to_latent_b = nn.Parameter(torch.randn(inner_latent_dim))
+
+        self.contextual_projection = nn.Linear(contextual_embed_dim, latent_heads * latent_heads)
+
+        self.to_pred = nn.Sequential(
+            nn.Linear(latent_heads, 1),
+            Rearrange('... 1 -> ...'),
+            nn.Softplus()
+        )
+
+    def forward(
+        self,
+        seq,
+        *,
+        aa_embed,
+        contextual_embed,
+        aa_mask = None,
+        target = None,
+        return_corr_coef = False
+    ):
+        latent_heads = self.latent_heads
+        _, seq_embed = self.enformer(seq, return_embeddings = True)
+
+        # project both embeddings into shared latent space
+
+        seq_latent = einsum('b n d, d e -> b n e', seq_embed, self.seq_embed_to_latent_w)
+        seq_latent = seq_latent + self.seq_embed_to_latent_b
+
+        seq_latent = rearrange(seq_latent, 'b n (h d) -> b h n d', h = latent_heads)
+
+        aa_latent = einsum('b n d, d e -> b n e', aa_embed, self.aa_seq_embed_to_latent_w)
+        aa_latent = aa_latent + self.aa_seq_embed_to_latent_b
+
+        aa_latent = rearrange(aa_latent, 'b n (h d) -> b h n d', h = latent_heads)
+
+        aa_latent, seq_latent = map(l2norm, (aa_latent, seq_latent))
+
+        # fine grained interaction between dna and protein sequences
+        # FILIP https://arxiv.org/abs/2111.07783
+
+        interactions = einsum('b h i d, b h j d -> b h i j', seq_latent, aa_latent)
+
+        # use mean pooling along amino acid sequence length
+
+        if exists(aa_mask):
+            aa_mask = rearrange(aa_mask, 'b j -> b 1 1 j')
+            interactions = interactions.masked_fill(aa_mask, 0.)
+
+        interactions = reduce(interactions, 'b h i j -> b i h', 'mean')
+
+        # derive contextual projection
+
+        hyper_weights = self.contextual_projection(contextual_embed)
+        hyper_weights = rearrange(hyper_weights, 'b (i o) -> b i o', i = int(math.sqrt(hyper_weights.shape[-1])))
+
+        # project interactions with hyper weights
+
+        logits = einsum('b n d, b d e -> b n e', interactions, hyper_weights)
+        pred = self.to_pred(logits)
+
+        if exists(target):
+            if return_corr_coef:
+                return pearson_corr_coef(pred, target)
+
+            return poisson_loss(pred, target)
+
+        return pred
