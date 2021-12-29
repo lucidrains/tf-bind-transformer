6c7d9a1e2047543445a1cdeb1bb6847c60b46dc4
Phil Wang <lucidrains@gmail.com>
2021-12-29 15:52:02 -0800

    add necessary helper functions for adam with weight decay

---
+++ tf_bind_transformer/training_utils.py
@@ -0,0 +1,17 @@
+from torch.optim import AdamW
+
+def separate_weight_decayable_params(params):
+    no_wd_params = set([param for param in params if param.ndim < 2])
+    wd_params = set(params) - no_wd_params
+    return wd_params, no_wd_params
+
+def get_optimizer(params, lr = 3e-4, wd = 1e-1):
+    params = set(params)
+    wd_params, no_wd_params = separate_weight_decayable_params(params)
+
+    param_groups = [
+        {'params': list(wd_params)},
+        {'params': list(no_wd_params), 'weight_decay': 0},
+    ]
+
+    return AdamW(param_groups, lr = lr, weight_decay = wd)
