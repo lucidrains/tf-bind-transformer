135f8efde31324183add81d0ebdf0f799e38b74b
Phil Wang <lucidrains@gmail.com>
2021-12-30 12:33:03 -0800

    allow for filtering by requires_grad of parameters

--- tf_bind_transformer/training_utils.py
+++ tf_bind_transformer/training_utils.py
@@ -5,7 +5,10 @@ def separate_weight_decayable_params(params):
     wd_params = set(params) - no_wd_params
     return wd_params, no_wd_params
 
-def get_optimizer(params, lr = 3e-4, wd = 1e-1):
+def get_optimizer(params, lr = 3e-4, wd = 1e-1, filter_by_requires_grad = False):
+    if filter_by_requires_grad:
+        params = list(filter(lambda t: t.requires_grad, params))
+
     params = set(params)
     wd_params, no_wd_params = separate_weight_decayable_params(params)
 
