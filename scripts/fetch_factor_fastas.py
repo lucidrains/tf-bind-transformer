2861c89bf8a34d19983ef8cbba936dda2de8ff91
Phil Wang <lucidrains@gmail.com>
2022-01-10 13:20:54 -0800

    handle multi-factor complexes

--- scripts/fetch_factor_fastas.py
+++ scripts/fetch_factor_fastas.py
@@ -5,6 +5,25 @@ import pandas as pd
 UNIPROT_URL = 'http://www.uniprot.org'
 REMAP_BED_PATH = './remap2022_crm_macs2_hg38_v1_0.bed'
 
+GENE_IDENTIFIER_MAP = dict(
+    RXR = 'RXRA'
+)
+
+def parse_gene_name(name):
+    if '-' not in name:
+        return (name,)
+
+    first, *rest = name.split('-')
+
+    parsed_rest = []
+
+    for name in rest:
+        if len(name) == 1:
+            name = f'{first[:-1]}{name}'
+        parsed_rest.append(name)
+
+    return tuple([first, *parsed_rest])
+
 def uniprot_mapping(fromtype, totype, identifier):
     params = {
         'from': fromtype,
@@ -16,45 +35,48 @@ def uniprot_mapping(fromtype, totype, identifier):
     response = requests.get(f'{UNIPROT_URL}/mapping', params = params)
     return response.text
 
-# load bed file and get all unique targets from column 3
+if __name__ == '__main__':
+    # load bed file and get all unique targets from column 3
 
-df = pd.read_csv(REMAP_BED_PATH, sep = '\t', header = None)
-genes = set([target for targets in df[3] for target in targets.split(',')])
+    df = pd.read_csv(REMAP_BED_PATH, sep = '\t', header = None)
+    genes = set([target for targets in df[3] for target in targets.split(',')])
 
-print(f'{len(genes)} factors found')
+    print(f'{len(genes)} factors found')
 
-# load all saved fasta files, so can resume gracefully
+    # load all saved fasta files, so can resume gracefully
 
-fasta_files = [str(path) for path in Path('./').glob('*.fasta')]
-processed_genes = set([*map(lambda t: str(t).split('.')[0], fasta_files)])
+    fasta_files = [str(path) for path in Path('./').glob('*.fasta')]
+    processed_genes = set([*map(lambda t: str(t).split('.')[0], fasta_files)])
 
-for gene_name in genes:
-    if gene_name in processed_genes:
-        continue
-
-    # fetch uniprot id based on gene id
-
-    uniprot_resp = uniprot_mapping('GENENAME', 'ID', gene_name)
-
-    # only get the human ones (todo: make species agnostic)
-
-    entries = list(filter(lambda t: '_HUMAN' in t, uniprot_resp.split('\n')))
-    entries = list(map(lambda t: t.split('\t')[1], entries))
-
-    if len(entries) == 0:
-        print(f'no entries found for {gene_name}')
-        continue
-
-    # save all hits
-
-    for entry in entries:
-        response = requests.get(f'{UNIPROT_URL}/uniprot/{entry}.fasta')
-
-        if response.status_code != 200:
-            print(f'<{response.status_code}> error fetching fasta file from gene {gene_name} {entry}')
+    for unparsed_gene_name in genes:
+        if gene_name in processed_genes:
             continue
 
-        with open(f'./{gene_name}.{entry}.fasta', 'w') as f:
-            f.write(response.text)
+        for gene_name in parse_gene_name(unparsed_gene_name):
+            # fetch uniprot id based on gene id
 
-    print(f'gene {gene_name} written')
+            gene_name = GENE_IDENTIFIER_MAP.get(gene_name, gene_name)
+            uniprot_resp = uniprot_mapping('GENENAME', 'ID', gene_name)
+
+            # only get the human ones (todo: make species agnostic)
+
+            entries = list(filter(lambda t: '_HUMAN' in t, uniprot_resp.split('\n')))
+            entries = list(map(lambda t: t.split('\t')[1], entries))
+
+            if len(entries) == 0:
+                print(f'no entries found for {gene_name}')
+                continue
+
+            # save all hits
+
+            for entry in entries:
+                response = requests.get(f'{UNIPROT_URL}/uniprot/{entry}.fasta')
+
+                if response.status_code != 200:
+                    print(f'<{response.status_code}> error fetching fasta file from gene {gene_name} {entry}')
+                    continue
+
+                with open(f'./{gene_name}.{entry}.fasta', 'w') as f:
+                    f.write(response.text)
+
+            print(f'gene {gene_name} written')
