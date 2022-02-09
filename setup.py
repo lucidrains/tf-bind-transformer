7f984f6ff5b9680381c7f5c87fe35e7c2ca9b135
Phil Wang <lucidrains@gmail.com>
2022-02-09 13:39:30 -0800

    add sentencepiece, for prot-albert

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'tf-bind-transformer',
   packages = find_packages(exclude=[]),
-  version = '0.0.44',
+  version = '0.0.45',
   license='MIT',
   description = 'Transformer for Transcription Factor Binding',
   author = 'Phil Wang',
@@ -26,6 +26,7 @@ setup(
     'logavgexp-pytorch',
     'polars',
     'python-dotenv',
+    'sentencepiece',
     'torch>=1.6',
     'transformers>=4.0',
     'tqdm'
