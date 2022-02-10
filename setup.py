83a8fc887b90d20f9c215208641696aca1d8145a
Phil Wang <lucidrains@gmail.com>
2022-02-10 10:24:37 -0800

    fix another potential issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'tf-bind-transformer',
   packages = find_packages(exclude=[]),
-  version = '0.0.48',
+  version = '0.0.49',
   license='MIT',
   description = 'Transformer for Transcription Factor Binding',
   author = 'Phil Wang',
--- tf_bind_transformer/data.py
+++ tf_bind_transformer/data.py
@@ -565,7 +565,7 @@ def collate_dl_outputs(*dl_outputs):
         if isinstance(entry[0], torch.Tensor):
             entry = torch.cat(entry, dim = 0)
         else:
-            entry = (sub_el for el in entry for sub_el in el)
+            entry = tuple([sub_el for el in entry for sub_el in el])
         ret.append(entry)
     return tuple(ret)
 
@@ -575,6 +575,6 @@ def cycle(loader):
             yield data
 
 def get_dataloader(ds, cycle_iter = False, **kwargs):
-    dl = DataLoader(ds, collate_fn = collate_fn, **kwargs)
+    dl = DataLoader(ds, collate_fn = collate_fn, drop_last = True, **kwargs)
     wrapper = cycle if cycle_iter else iter
     return wrapper(dl)
