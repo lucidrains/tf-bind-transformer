a1c64fc9777bcfa18d03d1768c734fa7f9d83daf
Phil Wang <lucidrains@gmail.com>
2022-02-09 16:31:22 -0800

    add joint cross attention

---
+++ tf_bind_transformer/attention.py
@@ -0,0 +1,85 @@
+import torch
+from torch import nn
+from einops import rearrange
+from torch import einsum
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+class JointCrossAttention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        heads = 8,
+        dim_head = 64,
+        context_dim = None
+    ):
+        super().__init__()
+        context_dim = default(context_dim, dim)
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = dim_head * heads
+
+        self.to_qk = nn.Linear(dim, inner_dim, bias = False)
+        self.context_to_qk = nn.Linear(context_dim, inner_dim, bias = False)
+
+        self.to_v = nn.Linear(dim, inner_dim, bias = False)
+        self.context_to_v = nn.Linear(context_dim, inner_dim, bias = False)
+
+        self.to_out = nn.Linear(inner_dim, dim)
+        self.context_to_out = nn.Linear(inner_dim, context_dim)
+
+    def forward(
+        self,
+        x,
+        context,
+        mask = None,
+        context_mask = None
+    ):
+        b, i, j, h, device = x.shape[0], x.shape[-2], context.shape[-2], self.heads, x.device
+
+        # get shared query/keys and values for sequence and context
+
+        qk, v = self.to_qk(x), self.to_v(x)
+        context_qk, context_v = self.context_to_qk(context), self.context_to_v(context)
+
+        # split out head
+
+        qk, context_qk, v, context_v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (qk, context_qk, v, context_v))
+
+        # get similarities
+
+        sim = einsum('b h i d, b h j d -> b h i j', qk, context_qk) * self.scale
+
+        # mask
+
+        if exists(mask) or exists(context_mask):
+            mask = default(mask, torch.ones((b, i), device = device).bool())
+            context_mask = default(context_mask, torch.ones((b, j), device = device).bool())
+
+            attn_mask = rearrange(mask, 'b i -> b 1 i 1') * rearrange(context_mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~attn_mask, -torch.finfo(sim.dtype).max)
+
+        # get attention along both sequence length and context length dimensions
+        # shared similarity matrix
+
+        attn = sim.softmax(dim = -1)
+        context_attn = sim.softmax(dim = -2)
+
+        # src sequence aggregates values from context, context aggregates values from src sequence
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, context_v)
+        context_out = einsum('b h j i, b h j d -> b h i d', context_attn, v)
+
+        # merge heads and combine out
+
+        out, context_out = map(lambda t: rearrange(t, 'b h n d -> b n (h d)'), (out, context_out))
+
+        out = self.to_out(out)
+        context_out = self.context_to_out(context_out)
+
+        return out, context_out
