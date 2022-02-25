fe9d0d20499404b190a06cb1a013a2d338118ea0
Phil Wang <lucidrains@gmail.com>
2022-02-24 16:50:03 -0800

    fix issue with bigwig dataset and polars filtering bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'tf-bind-transformer',
   packages = find_packages(exclude=[]),
-  version = '0.0.95',
+  version = '0.0.97',
   license='MIT',
   description = 'Transformer for Transcription Factor Binding',
   author = 'Phil Wang',
