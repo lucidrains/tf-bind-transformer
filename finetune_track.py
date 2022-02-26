e4105c7980d2ee837eb0b769ec3f76c0bbc03a10
Phil Wang <lucidrains@gmail.com>
2022-02-26 11:55:29 -0800

    show how to override heads on enformer, for training tracks without protein and contextual embeds

--- finetune_track.py
+++ finetune_track.py
@@ -42,8 +42,11 @@ model = AdapterModel(
     use_aa_embeds = True,
     use_free_text_context = True,
     free_text_embed_method = 'mean_pool',
-    aa_embed_encoder = 'protalbert',
-    use_corr_coef_loss = True               # use 1 - pearson_corr_coef loss
+    aa_embed_encoder = 'esm',
+    finetune_output_heads = dict(
+        human = 12,
+        mouse = 24
+    )
 ).cuda()
 
 
